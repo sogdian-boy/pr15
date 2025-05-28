0b851868e67137fdce099a033fafeff0c815fa9c
Arseniy1233242434 <senyakuzmin29@gmail.com>
2025-05-28 09:32:54 +0500

    update

--- app/src/main/java/com/example/pr15_isomatov_h/Login.kt
+++ app/src/main/java/com/example/pr15_isomatov_h/Login.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,34 @@ class Login : AppCompatActivity() {
     }
 
     fun goMain(view: View) {
-        val intent = Intent(this@Login,Main::class.java)
-        startActivity(intent)
-        finish()
+        val User = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val Pass = findViewById<EditText>(R.id.editTextTextPassword)
+
+        when {
+            User.text.isEmpty()->{
+                val alert = AlertDialog.Builder(this)
+                    .setTitle("Ошибка")
+                    .setMessage("Заполните поле: Email")
+                    .setPositiveButton("Ok",null)
+                    .create()
+                    .show()
+                User.requestFocus()
+            }
+
+            Pass.text.isEmpty()->{
+                val alert = AlertDialog.Builder(this)
+                    .setTitle("Ошибка")
+                    .setMessage("Заполните поле: Пароль")
+                    .setPositiveButton("Ok",null)
+                    .create()
+                    .show()
+                Pass.requestFocus()
+            }
+            else->{
+                var intend = Intent(this@Login,Main::class.java)
+                startActivity(intend)
+                finish()
+            }
+    }
     }
 }
---
+++ app/src/main/java/com/example/pr15_isomatov_h/ui/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.pr15_isomatov_h.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.pr15_isomatov_h.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
