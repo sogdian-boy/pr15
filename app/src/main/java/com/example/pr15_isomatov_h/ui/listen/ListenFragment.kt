045e5a2e83c8a1c6c6f12753f8d0637699b50067
Hamid <tarssvisionkira@gmail.com>
2025-05-28 09:45:32 +0500

    update

---
+++ app/src/main/java/com/example/pr15_isomatov_h/ui/listen/ListenFragment.kt
@@ -0,0 +1,26 @@
+package com.example.pr15_isomatov_h.ui.listen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.pr15_isomatov_h.databinding.FragmentDashboardBinding
+
+class ListenFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
